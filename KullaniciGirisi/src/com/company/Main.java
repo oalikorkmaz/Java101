febc6c09ff80b056b0d4816eb55025bafaa8c9a5
Onur Ali Korkmaz <53351567+oalikorkmaz@users.noreply.github.com>
2022-03-31 21:43:09 +0300

    Add files via upload

---
+++ KullaniciGirisi/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+        System.out.print("Kullanıcı adını giriniz: ");
+        String kullanici = scan.nextLine();
+        System.out.print("Şifreyi giriniz: ");
+        String sifre = scan.nextLine();
+
+    if(kullanici.equals("onur") && sifre.equals("1234"))
+            System.out.println("Giriş Başarılı.");
+        else
+        System.out.println("Kullanıcı adı veya şifre yanlış.");
+
+    }
+
+    }
+
